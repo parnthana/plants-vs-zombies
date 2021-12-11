84ed7ad87f8bae5ce709a8edd61d710065a0fd6a
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2021-12-11 23:25:08 +0700

    add Wallnut

---
+++ src/entity/plant/Wallnut.java
@@ -0,0 +1,32 @@
+package entity.plant;
+
+import entity.Plant;
+import javafx.scene.layout.Pane;
+import logic.GameController;
+
+public class Wallnut extends Plant {
+    // Constructor
+    public Wallnut(int x, int y, int row, int col) {
+        super(x, y, 60, 75, "/assets/walnut_full_life.gif", 400, row, col);
+        this.path = getClass().getResource("/assets/walnut_full_life.gif").toString();
+    }
+
+    // Method
+    public void checkHealthPoint() {
+        if (getHealthpoint() <= 0) {
+            setHealthpoint(0);
+            GameController.allPlants.remove(this);
+            image.setVisible(false);
+            image.setDisable(true);
+        }
+    }
+
+    @Override
+    public void attacking(Pane pane) {
+    }
+
+    @Override
+    public void attacking() {
+    }
+}
+
