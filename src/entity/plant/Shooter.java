d7b04507630a67d68fb60ddcda216acd64e2dd76
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2021-12-11 22:41:15 +0700

    add Shooter

---
+++ src/entity/plant/Shooter.java
@@ -0,0 +1,71 @@
+package entity.plant;
+
+import entity.Plant;
+
+import entity.Zombie;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import logic.GameController;
+
+public class Shooter extends Plant {
+
+    // Fields
+    protected Timeline shooterTimeline;
+    protected int lane;
+
+    // Constructor
+    public Shooter(int x, int y, String path, int healthpoint, int width, int height, int row, int column) {
+        super(x, y, width, height, path, healthpoint, column, row);
+        this.lane = column;
+    }
+
+    // Methods
+    @Override
+    public void attacking(Pane pane) {
+        Timeline peaShooter = new Timeline(new KeyFrame(Duration.seconds(2), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                synchronized (GameController.allZombies) {
+                    for (Zombie z : (Iterable<Zombie>) GameController.allZombies) {
+                        if (z.getLane() == getShooterLane() && getX() <= z.getX()) {
+                            int peaStartX = getX() + 50;
+                            int peaStartY = getY() + 25;
+                            Pea p = new Pea(peaStartX, peaStartY, getX() + 50, row);
+                            p.buildImage(pane);
+                            p.shootPea();
+                            checkHp();
+                        }
+                    }
+                }
+            }
+        }));
+        peaShooter.setCycleCount(Timeline.INDEFINITE);
+        peaShooter.play();
+        this.shooterTimeline = peaShooter;
+        GameController.animationTimelines.add(peaShooter);
+    }
+
+    @Override
+    public void attacking() {
+    }
+
+    public Timeline getShooterTimeline() {
+        return shooterTimeline;
+    }
+
+    public int getShooterLane() {
+        return lane;
+    }
+
+    public void checkHp() {
+        if (getHealthpoint() <= 0) {
+            endAnimation(shooterTimeline);
+        }
+    }
+
+}
+
