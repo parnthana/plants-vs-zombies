aa823dd193948156678977ec9889a3bb6fac2680
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2021-12-11 23:24:39 +0700

    add Repeater

---
+++ src/entity/plant/Repeater.java
@@ -0,0 +1,48 @@
+package entity.plant;
+
+import entity.Pea;
+import entity.Zombie;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import logic.GameController;
+
+
+public class Repeater extends Shooter {
+
+    // Constructor
+    public Repeater(int x, int y, int row, int col) {
+        super(x, y, "/assets/repeater.gif", 150, 60, 62, row, col);
+        this.path = getClass().getResource("/assets/repeater.gif").toString();
+
+    }
+
+    // Methods
+    @Override
+    public void attacking(Pane pane) {
+        Timeline peaShooter = new Timeline(new KeyFrame(Duration.seconds(2), event -> {
+            synchronized (GameController.allZombies) {
+                for (Object zombie : GameController.allZombies) {
+                    Zombie z = (Zombie) zombie;
+                    if (z.getLane() == getShooterLane() && getX() <= z.getX()) {
+                        int pea1StartX = getX() + 50;
+                        int pea2StartX = getX() - 20;
+                        int peaStartY = getY() + 26;
+                        Pea p1 = new Pea(pea1StartX, peaStartY, getX() + 50, row);
+                        Pea p2 = new Pea(pea2StartX, peaStartY, getX() + 50, row);
+                        p1.buildImage(pane);
+                        p2.buildImage(pane);
+                        p1.shootPea();
+                        p2.shootPea();
+                        checkHealthPoint();
+                    }
+                }
+            }
+        }));
+        peaShooter.setCycleCount(Timeline.INDEFINITE);
+        peaShooter.play();
+        shooterTimeline = peaShooter;
+        GameController.animationTimelines.add(peaShooter);
+    }
+}
