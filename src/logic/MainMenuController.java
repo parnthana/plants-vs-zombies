9605a441d003ce8a4e346acc2eec7a6eeebbf66e
Akkharawat Burachokviwat <Akkharawatbct@gmail.com>
2021-12-14 15:09:20 +0700

    addClassMainMenuController

---
+++ src/logic/MainMenuController.java
@@ -0,0 +1,32 @@
+package logic;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MainMenuController {
+    @FXML
+    private AnchorPane mainRoot;
+    @FXML
+    private Button startGame;
+    @FXML
+    private ImageView startgame;
+    @FXML
+    private Button startGame1;
+    @FXML
+    private ImageView selectLevel;
+    @FXML
+    private ImageView loadGame;
+    @FXML
+    private ImageView exitGame;
+}
